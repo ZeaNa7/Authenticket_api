2d3f3fa6f0e9a2306b42dcfaf1f600b22fec8dc2
Ana <niquita3@live.com>
2023-06-12 14:05:21 +0200

    ♻️(app): add apollo server

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ import { graphqlHTTP } from "express-graphql";
 import Express from "express";
 import cors from "cors";
 import resolvers from "./resolvers.js";
+import { ApolloServer } from "apollo-server";
 
 const app = Express();
 const typeDefs = importSchema("./schema.graphql");
@@ -21,6 +22,13 @@ app.use("/graphql", graphqlHTTP({
 })
 );
 
-app.listen(4000, () => {
-  console.log("Express GraphQL Server Now Running On localhost")
+const server = new ApolloServer({ typeDefs, resolvers });
+
+server.listen().then(({ url }) => {
+  console.log(`Server ready at ${url}`);
 });
+
+
+// app.listen(4000, () => {
+//   console.log("Express GraphQL Server Now Running On localhost")
+// });
